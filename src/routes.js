6621e8a9320a8fb58f065fdaabc3ff1bec5d3dda
rodrigoogalde <rjogalde@uc.cl>
2024-12-05 12:56:25 -0300

    Add example route for API request handling in Plates API

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const plates = require('./routes/plates');
 const sites = require('./routes/sites');
 const brands = require('./routes/brands');
 const users = require('./routes/users');
+const example = require('./routes/example');
 const jwtMiddleware = require('koa-jwt');
 const dotenv = require('dotenv');
 
@@ -14,6 +15,7 @@ const router = new Router();
 router.use('/sites',  sites.routes());
 router.use('/brands', brands.routes());
 router.use('/users', users.routes());
+router.use('/example', example.routes());
 
 router.use(jwtMiddleware({ secret: process.env.JWT_SECRET }));
 
